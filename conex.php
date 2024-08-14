68606418cac9b307a7de7f27c3a89c4b4fc0b097
Dougals A. <dodo.americ@gmail.com>
2024-08-14 19:52:54 -0300

    removi Senha

--- conex.php
+++ conex.php
@@ -1,7 +1,7 @@
 <?php 
     $host = 'localhost';
     $user = 'root';
-    $password = 'bancoDeDados@1';
+    $password = '';
     $database =  'teste';
 
     $con = mysqli_connect($host, $user, $password, $database);
