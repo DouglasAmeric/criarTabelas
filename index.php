9f99f100f09168bc8a6063367f8dd3c4e08c1d59
Dougals A. <dodo.americ@gmail.com>
2024-08-16 12:42:39 -0300

    nada

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
     <form action="index.php" method="post">
         <label for="table_name">Nome da Tabela</label>
         <input type="text" name="table_name">
+        <label for="row_">Coluna</label>
+        <input type="text" name="row_">
+        <select name="valor" id="valor">
+            <option value="int">int</option>
+            <option value="char">char</option>
+        </select>
         <button type="submit">Criar tabela</button>
     </form>
 </body>
@@ -19,12 +25,18 @@
 include("conex.php");
 
     $nome = isset($_POST['table_name']) ? trim($_POST['table_name']) : '';
+    $coluna = isset($_POST['row_']) ? trim($_POST['row_']) : '';
+    $valor = isset($_POST['valor']) ? trim($_POST['valor']) : '';
 
+    var_dump($coluna);
     
-    if(preg_match('/^[a-zA-Z0-9_]+$/', $nome)) {
+
+    if(preg_match('/^[a-zA-Z0-9_]+$/', $nome) && preg_match('/^[a-zA-Z0-9_]+$/', $coluna)){
         
         $create = "CREATE TABLE `$nome`(
-            id INT AUTO_INCREMENT PRIMARY KEY)";
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            $coluna $valor NOT NULL
+            )";
 
         if(mysqli_query($con,$create)){
             echo "Deu certo";
